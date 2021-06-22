fd6e68bb8e7693a0a578b2af4e3cbcd1616c3654
marmooo <marmooo@users.noreply.github.com>
2021-06-23 01:13:26 +0900

    fix path

--- mod.js
+++ mod.js
@@ -1,9 +1,11 @@
+import { fromFileUrl } from "http://deno.land/std/path/mod.ts";
 import { readLines } from "https://deno.land/std/io/mod.ts";
 
 class YomiDict {
   static async load() {
     const dict = {};
-    const fileReader = await Deno.open("./yomi.csv");
+    const url = fromFileUrl(new URL("./yomi.csv", import.meta.url));
+    const fileReader = await Deno.open(url);
     for await (const line of readLines(fileReader)) {
       const arr = line.split(",");
       const word = arr[0];
