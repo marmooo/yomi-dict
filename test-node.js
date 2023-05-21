f7b26113163963dc57d1561f3884b3dde1b5420b
marmooo <marmooo@users.noreply.github.com>
2023-05-21 16:43:05 +0900

    remove unused file

--- test-node.js
+++
@@ -1,8 +0,0 @@
-const YomiDict = require(".");
-
-async function main() {
-  const dict = await YomiDict.load();
-  console.log("学 --> " + dict.get("学"));
-  console.log("学校 --> " + dict.get("学校"));
-}
-main();
