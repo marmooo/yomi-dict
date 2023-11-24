c3667130a2ef165182d0eef765b0d098d9a5681d
marmooo <marmooo@users.noreply.github.com>
2023-11-24 13:25:08 +0900

    improve API

--- mod.js
+++ mod.js
@@ -1,39 +1,31 @@
 import { readLines } from "https://deno.land/std/io/mod.ts";
 
-class YomiDict {
-  static async fetch(url) {
-    const dict = await fetch(url)
-      .then((response) => response.text())
-      .then((text) => {
-        const d = {};
-        text.trimEnd().split("\n").forEach((line) => {
-          const arr = line.split(",");
-          const word = arr[0];
-          const yomis = arr.slice(1);
-          d[word] = yomis;
-        });
-        return d;
-      }).catch((e) => {
-        console.log(e);
-      });
+export class YomiDict {
+  static async fetch(url, options) {
+    const response = await fetch(url, options);
+    const text = await response.text();
+    const dict = {};
+    text.trimEnd().split("\n").forEach((line) => {
+      const arr = line.split(",");
+      const word = arr[0];
+      const yomis = arr.slice(1);
+      dict[word] = yomis;
+    });
     const yomiDict = new YomiDict();
     yomiDict.dict = dict;
     return yomiDict;
   }
 
-  static async load(filepath) {
+  static async load(filepath, options) {
     const dict = {};
-    if (!filepath) {
-      filepath = "./yomi-dict/yomi.csv";
-    }
-    const fileReader = await Deno.open(filepath);
-    for await (const line of readLines(fileReader)) {
+    const file = await Deno.open(filepath, options);
+    for await (const line of readLines(file)) {
       const arr = line.split(",");
       const word = arr[0];
       const yomis = arr.slice(1);
       dict[word] = yomis;
     }
-    fileReader.close();
+    file.close();
     const yomiDict = new YomiDict();
     yomiDict.dict = dict;
     return yomiDict;
@@ -47,5 +39,3 @@ class YomiDict {
     return this.dict[word];
   }
 }
-
-export { YomiDict };
