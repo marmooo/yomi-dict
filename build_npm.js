2292c6146510bee27204df7a0a7cc9328a299b37
marmooo <marmooo@users.noreply.github.com>
2024-08-12 19:52:30 +0900

    0.1.8

--- build_npm.js
+++ build_npm.js
@@ -14,7 +14,7 @@ await build({
   },
   package: {
     name: "yomi-dict",
-    version: "0.1.7",
+    version: "0.1.8",
     description: "Kanji reading dictionary.",
     license: "Apache-2.0",
     "main": "mod.js",
